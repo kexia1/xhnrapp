f4fc87bc6b4b3e70fc79ac61b1edd02490b29344
zhiyin <tql857@126.com>
2023-12-17 13:00:03 +0800

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public/")
+        }
+        google()
+        mavenLocal()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0-beta05" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
